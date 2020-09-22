eb087ed093885b878da4982e30413a1b06e99d69
gondigesu <godigesu@gmail.com>
2020-09-22 18:01:18 -0300

    third commit

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-import wait from 'waait';
+// import wait from 'waait';
 const menu=document.querySelector('.menu')
 const fotos=['foto1', 'foto2', 'foto3', 'foto4']
 
 const sticky=menu.offsetTop;
 
-async function changeImage(){
-    const img=document.querySelector('.img-razones')
-    await wait()
-    img.src=require("./img/foto2.jpg")
-    await wait(5000)
-    img.src=require("./img/foto3.jpg")
-    await wait(5000)
-    img.src=require("./img/foto1.jpg")
-    await wait(5000)
-    changeImage();
-}
+// async function changeImage(){
+//     const img=document.querySelector('.img-razones')
+//     await wait()
+//     img.src=require("./img/foto2.jpg")
+//     await wait(5000)
+//     img.src=require("./img/foto3.jpg")
+//     await wait(5000)
+//     img.src=require("./img/foto1.jpg")
+//     await wait(5000)
+//     changeImage();
+// }
 
 
 function stickyMenu() {
